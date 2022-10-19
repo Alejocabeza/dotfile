548cceebd73aacbf80fc6586a11b299b4ad70796
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2022-10-18 21:26:43 -0400

    configuration new

--- fish/config.fish
+++ fish/config.fish
@@ -10,5 +10,22 @@ alias ls "exa --all --group-directories-first"
 alias tree "exa -T"
 alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 
+# git
+alias g 'git'
+alias gi 'git init'
+alias ga 'git add .'
+alias gst 'git status'
+alias gc 'git commit -m'
+alias gcl 'git clone'
+alias gch 'git checkout'
+alias gp 'git push origin'
+alias gpl 'git pull origin'
+alias gs 'git stash'
+alias gsp 'git stash pop'
+alias gss 'git stash show'
+
+#neovim
+alias vim 'nvim'
+
 # Prompt
 starship init fish | source
