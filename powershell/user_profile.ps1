af8d74efeb6b4db424cec2780ddd88b00a11461a
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-11-09 18:20:51 -0400

    add powershell config

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,11 @@
+# Alias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias vim nvim
+
+# fnm
+fnm env --use-on-cd | Out-String | Invoke-Expression
+
+#starship
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
