9efd84081ad69a0bbd6ef568decc13d180dab780
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-11-10 02:23:50 -0400

    add new alias to powershell

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -2,6 +2,12 @@
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias vim nvim
+New-Alias -Name ch -Value changeDirectory
+New-Alias -Name www -Value changeWorkspaceDirectory
+New-Alias -Name dot -Value changeDotfileDirectory
+
+#imports
+Import-Module Terminal-Icons
 
 #fnm
 fnm env --use-on-cd | Out-String | Invoke-Expression
@@ -9,3 +15,17 @@ fnm env --use-on-cd | Out-String | Invoke-Expression
 #starship
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
+
+#fuctions
+function changeDirectory($link) {
+  $ruta = 'E:\' + $link
+  return Set-Location $ruta
+}
+
+function changeWorkspaceDirectory() {
+  return Set-Location 'E:\Documents\workspace'
+}
+
+function changeDotfileDirectory() {
+  return Set-Location '~\dotfile'
+}
