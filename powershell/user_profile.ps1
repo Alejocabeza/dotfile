ff33e27dcb02cb6459ae24cbe040b7bc908729aa
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-11-09 22:28:04 -0400

    fix powershell configuration

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -3,7 +3,8 @@ Set-Alias ll ls
 Set-Alias g git
 Set-Alias vim nvim
 
-# fnm fnm env --use-on-cd | Out-String | Invoke-Expression
+#fnm
+fnm env --use-on-cd | Out-String | Invoke-Expression
 
 #starship
 Invoke-Expression (&starship init powershell)
