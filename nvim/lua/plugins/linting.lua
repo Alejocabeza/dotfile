8379856cbe6b5d9a8a4bd9f8f2fdd153f9fcdcab
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-11-28 08:39:40 -0400

    fix formatting

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -1,14 +1,16 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      fish = { "fish" },
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
-    },
-  },
+	"mfussenegger/nvim-lint",
+	opts = {
+		linters_by_ft = {
+			fish = { "fish" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			html = { "eslint_d" },
+			css = { "eslint_d" },
+			python = { "pylint" },
+		},
+	},
 }
