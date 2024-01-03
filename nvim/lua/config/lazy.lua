27b710b18968ba1ac121596a69d3d8595d7b1b5a
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-01-03 09:19:58 -0400

    fix global settings neovim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,8 +17,14 @@ require("lazy").setup({
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
-		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.lang.yaml" },
+		-- { import = "lazyvim.plugins.extras.lang.python" },
+		-- Extra Coding
+		{ import = "lazyvim.plugins.extras.coding.codeium" },
+		-- Extra Editor
+		{ import = "lazyvim.plugins.extras.editor.aerial" },
 		-- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		-- { import = "lazyvim.plugins.extras.linting.eslint" },
 		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -44,6 +44,14 @@ return {
 		vim.g.loaded_netrwPlugin = 1
 		require("nvim-tree").setup(opts)
 	end,
+	init = function()
+		if vim.fn.argc(-1) == 1 then
+			local stat = vim.loop.fs_stat(vim.fn.argv(0))
+			if stat and stat.type == "directory" then
+				require("nvim-tree")
+			end
+		end
+	end,
 	keys = {
 		{ "nt", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle File Explorer" } },
 	},
